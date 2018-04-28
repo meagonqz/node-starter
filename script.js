b12501affe0de729be8727f34f37f9d0fb980a20
Meaghan Harty <mharty@localytics.com>
2018-04-28 16:36:09 -0400

    add prettier

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import "babel-polyfill";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-const test = fetch('https://jsonplaceholder.typicode.com/posts/1').then( response => response.json() ).then( json => console.log(json));
-
+const test = fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then(response => response.json())
+  .then(json => console.log(json));
